92851592956b0b5e15fb0d4c6af5311a0fc99309
Mejsun <mejsunalghoul@gmail.com>
2021-12-05 14:20:22 +0000

    Todolist
    Added delete button

--- src/assets/components/Todo/Todo.js
+++ src/assets/components/Todo/Todo.js
@@ -1,12 +1,38 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
+import '../../styles/Custom.scss'
 
 function Todo() {
 const [tasks, setTasks] = useState([]);
+const todoItem = useRef();
+
+function addTodoItem (e){
+    e.preventDefault() 
+    setTasks([...tasks, todoItem.current.value])
+    todoItem.current.value=''}
+
+function deleteItem(i){
+    const item = [...tasks]
+    const remove = item.splice(i,1)
+    setTasks(item)
+}
+
 
     return (
         <div>
-            <input type='text' onBlur={(e)=>{return (setTasks([...tasks, e.target.value]))}}/>
-            {tasks.map((item,i)=>{return(<div key={Math.random()}>{i+1}. {item}</div>)})}
+            <form>
+                <input type='text' ref={todoItem}/>
+                <button type='submit' onClick={(e) => {return (addTodoItem(e))}}>+</button>
+            </form>
+            {tasks.map((item,i)=>{return(
+            <div key={Math.random()} id={Math.random()} 
+            //className={item.completed==='true' ? 'completed' : ''}
+            >
+                <div>
+                {i+1}. {item}
+                <button type='button' onClick={()=>{deleteItem(i)}}>-</button>
+                <button type='button' onClick={()=>{return(console.log('completed'))}}>!</button>
+                </div>
+            </div>)})}
         </div>
     )
 }
