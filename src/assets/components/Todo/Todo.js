3c005026d109566b96050f0c09e652378e811bf7
Mejsun <mejsunalghoul@gmail.com>
2021-12-08 11:29:30 +0000

    Complete button working

--- src/assets/components/Todo/Todo.js
+++ src/assets/components/Todo/Todo.js
@@ -1,42 +1,48 @@
-import React, {useState, useRef} from 'react'
+import React, {useState} from 'react'
 import '../../styles/Custom.scss'
 
 function Todo() {
+const [value, setValue] = useState('');
 const [tasks, setTasks] = useState([]);
-const [state, setState] = useState('false')
-const todoItem = useRef();
 
-function addTodoItem (e){
-    e.preventDefault() 
-    if(todoItem.current.value!==''){
-    setTasks([...tasks, todoItem.current.value])
-    todoItem.current.value=''}}
+function addTodoItem (){
+    setTasks([...tasks, {text:value, isCompleted: false}])}
+
+function submitItem (e){
+    e.preventDefault(); 
+    if(!value) return;
+    addTodoItem(value);
+    setValue('')}
 
 function deleteItem(i){
-    const item = [...tasks]
-    const remove = item.splice(i,1)
-    setTasks(item)
-}
+    const items = [...tasks]
+    items.splice(i,1)
+    setTasks(items)}
+
 function completeItem(i){
-    setState(!state);
-}
+    const items = [...tasks]
+    if(items[i].isCompleted === false){items[i].isCompleted = true; console.log('Well done!')}
+    else{items[i].isCompleted = false}
+        setTasks(items);}
 
     return (
         <div className='container'>
             <form className='form'>
-                <input type='text' ref={todoItem} className='input'/>
-                <button type='submit' onClick={(e) => {return (addTodoItem(e))}} className='btn submit'><i class="fas fa-plus"></i></button>
+                <input type='text' value={value} className='input' onChange={e => setValue(e.target.value)}/>
+                <button type='submit' className='btn submit' onClick={submitItem}><i className="fas fa-plus"></i></button>
             </form>
             {tasks.map((item,i)=>{return(
-                <div key={Math.random()} id={Math.random()} className={`item ${!state ? 'done' : ''}`}>
-                    {i+1}. {item}
+                <div className={`item ${item.isCompleted  ? 'done' : ''}`} 
+                id={Math.random()} 
+                key={Math.random()}>
+                {i+1}. {item.text}
                     <div className='buttons'>
-                    <button type='button' onClick={()=>{deleteItem(i)}} className='btn delete'> <i className='fas fa-trash'></i></button>
-                    <button type='button' onClick={()=>{completeItem(i)}} className='btn complete'><i className='fas fa-check'></i></button>
+                        <button type='button' onClick={()=>{deleteItem(i)}} className='btn delete'> <i className='fas fa-trash'></i></button>
+                        <button type='button' onClick={() =>{completeItem(i)}} className='btn complete'><i className='fas fa-check'></i></button>
                     </div>
-                </div>)})}
+                </div>
+                )})}
         </div>
-    )
-}
+)}
 
 export default Todo
